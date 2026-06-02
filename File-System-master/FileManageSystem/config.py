4a4bb201d91491c4459691305170b406764681c4
rita31415926 <15940150912@163.com>
2026-06-02 14:30:12 +0800

    v1
    Signed-off-by: lidalaoganbu <2765010334@qq.com>

---
+++ File-System-master/FileManageSystem/config.py
@@ -0,0 +1,63 @@
+<<<<<<< HEAD
+# 状态栏颜色
+BAR_TIME_F = "33"
+BAR_TIME_B = "40"
+BAR_USER_F = "36"
+BAR_USER_B = "40"
+BAR_VERSION_F = "31"
+BAR_VERSION_B = "47"
+BAR_PATH_F = "37"
+BAR_PATH_B = "44"
+BAR_PROMPT_F = "32"
+BAR_PROMPT_B = "40"
+
+# 系统信息颜色
+SYS_INFO_F = "36"
+SYS_INFO_B = "40"
+
+# 登录界面颜色
+LOGIN_TITLE_F = "33"
+LOGIN_LABEL_F = "32"
+
+
+
+
+
+BLOCK_SIZE = 512  # 磁盘块大小Bytes
+BLOCK_NUM = 2560  # 磁盘块总数量
+
+SUPER_BLOCK_NUM = 2  # 超级块占用的块数
+INODE_BLOCK_NUM = 256  # 索引占用的块数
+DATA_BLOCK_NUM = BLOCK_NUM - SUPER_BLOCK_NUM - INODE_BLOCK_NUM
+
+INODE_BLOCK_START_ID = SUPER_BLOCK_NUM
+
+DATA_BLOCK_START_ID = SUPER_BLOCK_NUM + INODE_BLOCK_NUM + 1  # 数据块的起始地址
+
+INODE_SIZE = 512  # INODE占用的块区大小
+
+DISK_SIZE = BLOCK_SIZE * BLOCK_NUM  # 磁盘大小
+DISK_NAME = "../fms.pfs"
+DIR_NUM = 128  # 每个目录锁包含的最大文件数
+FREE_NODE_CNT = 50  # 超级块中空闲节点的最大块数
+FREE_BLOCK_CNT = 100  # 超级块中空闲数据块的最大块数
+
+BASE_NAME = "base"  # 根目录名
+
+FILE_TYPE = 0  # 文件类型
+DIR_TYPE = 1  # 目录类型
+
+ROOT_ID = 0
+ROOT = 'root'
+
+INIT_DIRS = ['root', 'home', 'etc']
+
+VERSION = "V 1.2"
+
+
+# color
+FILE_COLOR_F = "37"  # 文件名前景色
+FILE_COLOR_B = "40"  # 文件名背景色
+
+DIR_COLOR_F = "32"
+DIR_COLOR_B = "40"
---
+++ File-System-master/FileManageSystem/utils.py
@@ -0,0 +1,93 @@
+<<<<<<< HEAD
+=======
+"""
+author:Wenquan Yang
+time:2020/6/10 2:35
+content:各种工具脚本
+"""
+>>>>>>> remotes/origin/li
+import time
+import pickle
+from math import ceil
+from config import BLOCK_SIZE, ROOT_ID, VERSION
+
+
+def serializer(text: str) -> list:
+    """
+    输入文本，将其转换成按照block大小切分的块
+    :param text: 待序列化的文本
+    :return: list[b'',b'']
+    """
+    b_text = pickle.dumps(text)
+    block_num = int(ceil(len(b_text) / BLOCK_SIZE))  # 计算块数向上取整
+    yield from [b_text[BLOCK_SIZE * i:BLOCK_SIZE * (i + 1)] for i in range(block_num)]
+
+
+def split_serializer(b_obj: bytes) -> list:
+    """
+    输入字节流按照block大小切分
+    :param b_obj:
+    :return:
+    """
+    block_num = int(ceil(len(b_obj) / BLOCK_SIZE))  # 计算块数向上取整
+    yield from [b_obj[BLOCK_SIZE * i:BLOCK_SIZE * (i + 1)] for i in range(block_num)]
+
+
+def form_serializer(fp, block_num):
+    s = b''
+    for _ in range(block_num):
+        s += fp.read()
+    return s
+
+
+def check_auth(auth_id, user_id):
+    return auth_id == user_id or user_id == ROOT_ID
+
+
+def color(filename: str, front_color: str, back_color: str):
+    filenames = '\33[0;' + front_color + ';' + back_color + 'm' + filename + '\33[0m'
+    return filenames
+
+
+def bar(user_name, current_path):
+    time_now = time.strftime(" %H:%M:%S ", time.localtime())
+<<<<<<< HEAD
+    # 时间：字体颜色不变（黄色33），背景改为米白色（107亮白或103浅黄，这里用107）
+    time_now = color(time_now, "33", "107")
+    # 用户名@PFS：字体颜色不变（青色36），背景改为米白色（107）
+    user_name = color(' ' + user_name + '@PFS ', "36", "107")
+    # 版本信息：背景和字体颜色不变（红色前景31，白色背景47）
+    version = color(' ' + VERSION + ' ', "31", "47")
+    # 当前路径（如 >root）：背景绿色（42），字体红色（31）
+    current_path = color(' >' + current_path + ' ', "31", "42")
+    # 命令行提示符（> ）保持不变（绿色前景32，黑色背景40）
+    cmd_in = color('> ', "32", "40")
+
+=======
+    time_now = color(time_now, "30", "47")
+    user_name = color(' ' + user_name + '@PFS ', "37", "40")
+    current_path = color(' >' + current_path + ' ', "37", "44")
+    version = color(' ' + VERSION + ' ', "31", "42")
+    cmd_in = color('> ', "44", "40")
+>>>>>>> remotes/origin/li
+    print(time_now + user_name + version + current_path)
+    print(cmd_in, end="")
+
+
+<<<<<<< HEAD
+=======
+def logo():
+    LOGO1 = '\33[1;34;40m' + "    ____   ______ _____\n   / __ \ / ____// ___/\n"
+    LOGO2 = '\33[1;36;40m' + " / /_/ // /_    \__ \\" + "\n"
+    LOGO3 = '\33[1;32;40m' + "/ ____// __/   ___/ /\n/_/    /_/     /____/\n\n\33[0m"
+    print(LOGO1, LOGO2, LOGO3)
+
+
+>>>>>>> remotes/origin/li
+def line(func):
+    def return_func(*args):
+        print()
+        func(*args)
+        print()
+
+    return return_func
